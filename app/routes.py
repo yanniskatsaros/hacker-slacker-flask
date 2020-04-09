17598fe572d7625002229b6eb850a2d03bbf1f9c
Yannis Katsaros <yanniskatsaros@hotmail.com>
2020-04-08 22:30:14 -0500

    Use Link class instead of dict

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,20 @@
+from dataclasses import dataclass
+
 from flask import current_app as app
 from flask import render_template
+
+@dataclass
+class Link:
+    url: str
+    name: str
 
 @app.route('/')
 def home():
     """Landing page"""
     nav = [
-        {'name': 'Home', 'url': 'https://example.com/1'},
-        {'name': 'About', 'url': 'https://example.com/2'},
-        {'name': 'Contact', 'url': 'https://example.com/3'}
+        Link(name="Home", url="https://example.com/1"),
+        Link(name="About", url="https://example.com/2"),
+        Link(name="Pics", url="https://example.com/3"),
     ]
     return render_template(
         'home.html.jinja',
